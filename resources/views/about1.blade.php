c2dfb3743d0b515187b1a9c81027ba056c321fda
Saikat724 <talukdersaikat444@gmail.com>
2023-01-01 18:24:55 +0600

    Route setting

---
+++ resources/views/about1.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+    <h2 style="color:blue;">This is about page</h2>
+    <p style="color: blue;">First Number = {{ $a = 10 }}</p>
+    <p>Second Number = {{ $b = 20 }}</p>
+    <p>Total = {{ $c = $a + $b }}</p>
+</body>
+</html>
